03c3f6d75a0139a268f1c41e7814e139c248c147
yuan <987607481@qq.com>
2020-03-28 18:46:40 +0800

    18:46

--- src/app/report/salerticket/salerticket.component.ts
+++ src/app/report/salerticket/salerticket.component.ts
@@ -6,6 +6,7 @@ import { PagedListingComponentBase, PagedRequestDto } from '@shared/component-ba
 import {
 	UserServiceProxy ,QueryData,
 	TicketServiceProxy,
+	SalesByPayMethodResultDto
 	// SellerTicketResultDto,
 	// GetTicketsInput
 } from '@shared/service-proxies/service-proxies';
@@ -24,7 +25,7 @@ import * as moment from 'moment';
 })
 
 
-export class  SalerTicketComponent extends PagedListingComponentBase<''>
+export class  SalerTicketComponent extends PagedListingComponentBase<SalesByPayMethodResultDto>
 implements OnInit {
 
   constructor(
